331ba9804e427543628b4463e85f4bb6fc95b769
martina.mxd <martina.mxd@alibaba-inc.com>
2024-04-12 11:30:30 +0800

    Stack Custom

--- src/leetcode/editor/cn/DesignAStackWithIncrementOperationXXX1381.java
+++ src/leetcode/editor/cn/DesignAStackWithIncrementOperationXXX1381.java
@@ -58,21 +58,58 @@ public static void main(String[] args) {
 }
 //leetcode submit region begin(Prohibit modification and deletion)
 class CustomStack {
+    // 1。 直接使用双栈比较直接，就是保存一个元素总和，当递增的时候用备用栈倒出来，从倒到n-k的时候开始增加，然后再倒回去原来的栈
 
+    // 2。 其实这道题考察的是模拟的思想，用数组表示栈，会更直接一点，每次incre就遍历0-k对数组执行++就行，这样incre的复杂度是Ok
+
+    // 3。 有一个比较巧妙的方法就是使用前缀和数组的思想，用数组实现栈，保存一个addArr[i]表示在i位置需增加的值，这样每次increment只用增加addArr[k]位置的值
+    // push保存一个栈顶索引，topIndex，pop的时候需要拿到stack[topIndex]+addArr[topIndex]的值
+    // 又因为每次incre的时候只是增加了addArr[k],为了保证每次取到的值都是已经被加过的，所以每次pop以后，addArr[topIndex-1]位置也要增加addArr[topIndex]
+    // 这样看似有点难理解，但是想清楚了就很妙了，可以保证incre的时候复杂度都是O1
+
+    int [] stack;
+    int addArr[];
+
+    int topIndex;
+
+    // 直接写一下前缀和方式
     public CustomStack(int maxSize) {
-
+        addArr = new int[maxSize];
+        stack = new int[maxSize];
+        topIndex = -1;
     }
     
     public void push(int x) {
-
+        if(topIndex == stack.length-1){
+            return ;
+        }
+        stack[++topIndex] = x;
     }
     
     public int pop() {
-        return -1;
+        if(topIndex==-1){
+            return -1;
+        }
+
+        int addVal = addArr[topIndex];
+        int res = stack[topIndex]+addVal;
+
+        // 更新最新top索引处的add值
+        if(topIndex>=1){
+            addArr[topIndex-1] += addVal;
+        }
+        // 记得还原addArr[topIndex] =0
+        addArr[topIndex] =0;
+        topIndex--;
+        return res;
     }
     
     public void increment(int k, int val) {
-
+        // 这里要用topIndex和K比较
+        int actualK = Math.min(k,topIndex+1);
+        if(actualK >=1){
+            addArr[actualK-1] +=val;
+        }
     }
 }
 
